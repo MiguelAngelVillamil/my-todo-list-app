11be2c9eb212a74500cf6f0e315c5745b7dbe2e8
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-10-08 13:26:06 -0300

    first commit

--- src/components/FormToDo/FormToDo.jsx
+++ src/components/FormToDo/FormToDo.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function FormTodo() {
-	return(
-    <div>
-        FormTodo!
-    </div>
+export default function FormTodo(props) {
+
+	const [description, setDescription] = useState("");
+
+  const { handleAddItem } = props;
+
+  const handleSubmit = evento => {
+    evento.preventDefault();
+
+    handleAddItem({
+      done: false,
+      id: (+new Date()).toString(),
+      description
+    });
+
+    setDescription("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="todo-list">
+        <div className="file-input">
+          <input
+            type="text"
+            className="text"
+            value={description}
+            onChange={(evento) => setDescription(evento.target.value)}
+          />
+          <button
+            className="button pink"
+            disabled={description ? "" : "disabled"}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
   );
 };
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,26 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Checkbox from "../Checkbox/Checkbox.jsx";
 
 export default function TaskList() {
-	const [description, setDescription] = useState("");
-  return (
-    <form>
-      <div className="todo-list">
-        <div className="file-input">
-          <input
-            type="text"
-            className="text"
-            value={description}
-            onChange={(evento) => setDescription(evento.target.value)}
-          />
-          <button
-            className="button pink"
-            disabled={description ? "" : "disabled"}
-          >
-            Add
-          </button>
-        </div>
-      </div>
-    </form>
+	return(
+    <div>
+        TaskList!
+        <Checkbox />
+    </div>
   );
 };
+
